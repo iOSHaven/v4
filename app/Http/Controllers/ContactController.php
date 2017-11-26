a96f34b226500dc82a83fe9d56e605c71d8cb15b
Zebthewizard <burroughszeb@me.com>
2017-11-25 21:56:38 -0600

    fix again

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -16,7 +16,7 @@ class ContactController extends Controller
     public function send (Request $r) {
       $data = [];
       foreach ($r->all() as $key => $value) {
-        if ($key !== '_token' && $ke !== 'query_string') {
+        if ($key !== '_token' && $key !== 'query_string') {
           $data[str_replace("-", " ", $key)] = $value;
         }
       }
