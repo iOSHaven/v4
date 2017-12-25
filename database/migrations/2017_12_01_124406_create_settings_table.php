5d9cc3f0ff1e1adc7362133b7e7ba2c0b5821ace
Zebthewizard <burroughszeb@me.com>
2017-12-24 20:10:31 -0600

    santa particles

--- database/migrations/2017_12_01_124406_create_settings_table.php
+++ database/migrations/2017_12_01_124406_create_settings_table.php
@@ -30,6 +30,6 @@ class CreateSettingsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('announcements');
+        Schema::dropIfExists('settings');
     }
 }
