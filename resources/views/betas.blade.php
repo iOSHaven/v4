b1893bd3339a2732a3d8aeb43dc0562e8a16ec50
Zack <zackbaldwin148@gmail.com>
2018-06-17 13:11:14 -0400

    added mac os beta and games

--- resources/views/betas.blade.php
+++ resources/views/betas.blade.php
@@ -4,7 +4,10 @@
     <h1>iOS</h1>
     <ul class="fancy">
       <li >
-        iOS Betas: <a href="https://iosbetas.me/" target="_blank">GO!</a>
+        iOS 12: <a href="https://iosbetas.me/" target="_blank">GO!</a>
+      </li>
+      <li >
+        MAC OS Mojave: <a href="http://festyy.com/wH5U36" target="_blank">GO!</a>
       </li>
     </ul>
 </div>
