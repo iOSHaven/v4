0615dd537e8d72acc43eb06b8e7d521655919761
criticalthewizard <burroughszeb@me.com>
2018-10-30 12:55:03 -0500

    fix

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -88,7 +88,8 @@ $('document').ready(function () {
 $(document).ready(function() {
 
   // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function() {
+  $(".navbar-burger").on('click touch', function(e) {
+      e.preventDefault();
       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
       $(".navbar-burger").toggleClass("is-active");
       $(".navbar-menu").toggleClass("is-active");
