c1e86ece2cc514896bdb675167a80c6ba09fc8f4
criticalthewizard <burroughszeb@me.com>
2019-02-28 23:42:59 -0600

    google fonts mobile?

--- resources/views/layouts/redesign.blade.php
+++ resources/views/layouts/redesign.blade.php
@@ -123,10 +123,7 @@
 
 
     <!-- === F O N T S === -->
-    <link href="https://fonts.googleapis.com/css?family=Fira+Sans:900i,700|Martel:400,700" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css?family=Merriweather:900i|Amiko:400|Montserrat:600|Lora:400" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400|Ubuntu" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:900i|Amiko:400" rel="stylesheet" />
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-6jHF7Z3XI3fF4XZixAuSu0gGKrXwoX/w3uFPxC56OtjChio7wtTGJWRW53Nhx6Ev" crossorigin="anonymous">    <link href="{{ asset('css/normalize.css') }}" rel="stylesheet">
     
     <!-- === S T Y L E S === -->
