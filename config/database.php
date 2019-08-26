333a421b72d80ec9621d13ed122e9b04c557f5e4
Zeb <burroughszeb@me.com>
2019-08-25 21:48:56 -0500

    redis cache

--- config/database.php
+++ config/database.php
@@ -124,6 +124,13 @@ return [
             'database' => 0,
         ],
 
+        'cache' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 1,
+        ],
+
         'options' => [
             'password' => env('REDIS_PASSWORD', null),
             'ssl' => ['verify_peer' => false],
