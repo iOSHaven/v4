5196392c73f73065621c10f5a304b053fcde0e15
Zeb <burroughszeb@me.com>
2019-11-19 16:47:59 -0600

    temporarily remove non-auto ads.

--- resources/views/apps.blade.php
+++ resources/views/apps.blade.php
@@ -15,9 +15,8 @@ fbq('track', 'Search');
           @foreach($apps as $app)
             @component('components.applayout', $app->toArray())@endcomponent
             @if($loop->iteration == 3)
-              <div class="relative">
+              {{-- <div class="relative">
                 <div class="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center -z-1 {{ theme('bg-gray-100') }}">
-                  {{-- Please consider disabling ads. --}}
                 </div>
                 <!-- v4-search-bottom -->
                 <ins class="adsbygoogle"
@@ -26,7 +25,7 @@ fbq('track', 'Search');
                      data-ad-slot="5262456899"
                      data-ad-format="auto"
                      data-full-width-responsive="true"></ins>
-              </div>
+              </div> --}}
             @endif
           @endforeach
       </div>
