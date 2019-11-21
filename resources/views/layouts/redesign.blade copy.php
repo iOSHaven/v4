afe4dd817fa7ad3f4514adb7f8daf8ee6c1da25d
Zeb <burroughszeb@me.com>
2019-11-21 13:23:16 -0600

    tested pushed code and needed to fix layout file and logging.

--- resources/views/layouts/redesign.blade copy.php
+++ resources/views/layouts/redesign.blade copy.php
@@ -1,41 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>{{ config('app.name', 'IOS Haven') }}</title>
-  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  {{-- MOBILE FRIENDLY --}}
+<head>
+
+  !<title>{{ config('app.name', 'IOS Haven') }}</title>
+  !<meta name="csrf-token" content="{{ csrf_token() }}">
+
+  @if(empty($hide_meta))
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106909262-1"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+          dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'UA-106909262-1');
+      </script>
+  @endif
+
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, viewport-fit=cover">
 
-  {{-- SEARCH ENGINE FRIENDLY --}}
+  <meta name="application-name" content="IOS Haven">
+  <meta name="theme-color" content="{{ theme() === 'light' ? '#ffffff' : '#000000' }}">
   <meta name="description"
-      content="Discover hacked games, tweaked apps, jailbreaks and more. We offer tools to get started, links you should visit, and thousands of popular apps ready for download.">
+    content="We introduce people to the sideload and jailbreak IOS communities. We offer tools to get started, links you should visit, and hundreds of popular apps ready for download.">
   <meta name="keywords" content="iphone,jailbreak,sideload,hack,crack,signed,download,ipa,free">
   <meta name="robots" content="index, follow">
   <meta name="web_author" content="IOS Haven Development Team">
   <meta name="language" content="English">
 
   {{-- TWITTER OG PROPERTIES --}}
-  <meta property="og:title" content="{{ config('app.name') }}">
+  <meta property="og:title" content="{{ config('app.name', 'IOS Haven') }}">
   <meta property="og:type" content="article">
   <meta property="og:url" content="{{ $url ?? url('/') }}">
   <meta property="og:description" content="DOWNLOAD IPAS, SIGNED APPS, APPLE DEVELOPER BETAS, AND JAILBREAKS.">
   <meta property="og:image" content="/ios-banner.png">
 
-  {{-- WEB APPLICATION FRIENDLY --}}
-  <meta name="application-name" content="{{ config('app.name') }}">
-  <meta name="theme-color" content="{{ theme() === 'light' ? '#ffffff' : '#000000' }}">
-  <meta name="apple-mobile-web-app-title" content="{{ config('app.name') }}">
+  <!-- Apple specific -->
+  <meta name="apple-mobile-web-app-title" content="IOS Haven">
   <meta name="apple-mobile-web-app-capable" content="yes">
-  <meta name="apple-mobile-web-app-status-bar-style" content="{{ theme() === 'light' ? 'default' : 'black' }}" id="status-bar-style">
+  {{-- <meta name="apple-mobile-web-app-status-bar-style" content="black"> --}}
   <link rel="manifest" href="/manifest-{{ theme() }}.json">
+  <meta name="apple-mobile-web-app-status-bar-style" content="{{ theme() === 'light' ? 'default' : 'black' }}" id="status-bar-style">
+  {{-- <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"> --}}
 
-  {{-- SPLASH SCREENS --}}
-  @include('layouts.splash')
 
-  {{-- FAVICONS --}}
+
+
+  
+
+
+  <!-- === F A V I C O N S === -->
   <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png?v=QEMYzE9pb35555" sizes="180x180">
   <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png?v=QEMYzE9pb35555">
   <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png?v=QEMYzE9pb35555">
@@ -43,55 +62,87 @@
   <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg?v=QEMYzE9pb35555" color="#e81e1e">
   <link rel="shortcut icon" href="/favicons/favicon.ico?v=QEMYzE9pb35555">
 
-  {{-- FONTS --}}
+  <!-- === F O N T S === -->
   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
     integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css?family=Merriweather:900i|Amiko:400" rel="stylesheet" />
+  {{-- <script src="https://kit.fontawesome.com/c888111707.js"></script> --}}
 
-  {{-- STYLES --}}
+  <!-- === S T Y L E S === -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" rel="stylesheet">
+  {{-- <link href="{{ mix('/css/main.min.css') }}" rel="stylesheet"> --}}
   <link href="{{ mix('/css/redesign.min.css') }}" rel="stylesheet">
+  
 
-  {{-- GOOGLE ADSENSE --}}
+
+
+  <!-- === A D S E N S E   A D S === -->
+  {{-- <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> --}}
   @if(empty($hide_ads))
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
   @endif
-
-  {{-- REDDIT CONVERSION PIXEL --}}
-  <script>var now = Date.now();var i = new Image();i.src="https://alb.reddit.com/snoo.gif?q=CAAHAAABAAoACQAAAAmui0GsAA==&s=tmLAxiUDMEJKMclLiZGzuUt9BH8_1Z3ptx3Bh1LlzNs=&ts=" + now;</script>
-  <noscript><img height="1" width="1" style="display:none"src="https://alb.reddit.com/snoo.gif?q=CAAHAAABAAoACQAAAAmui0GsAA==&s=tmLAxiUDMEJKMclLiZGzuUt9BH8_1Z3ptx3Bh1LlzNs=" /></noscript>
-
-  {{-- FACEBOOK CONVERSION CODE --}}
-  <script>! function (f, b, e, v, n, t, s) {if (f.fbq) return;n = f.fbq = function () {n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)};if (!f._fbq) f._fbq = n;n.push = n;n.loaded = !0;n.version = '2.0';n.queue = [];t = b.createElement(e);t.async = !0;t.src = v;s = b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t, s)}(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');fbq('init', '362444877957386');fbq('track', 'PageView');
-    @yield('facebook-events')
-  </script>
-  <noscript><img height="1" width="1"src="https://www.facebook.com/tr?id=362444877957386&ev=PageView&noscript=1" /></noscript>
+ 
 
 
-  {{-- PER VIEW HEADER CODE --}}
+  @if(empty($hide_meta))
+      <!-- Reddit Conversion Pixel -->
+      <script>
+        var now = Date.now();
+        var i = new Image();
+        i.src =
+          "https://alb.reddit.com/snoo.gif?q=CAAHAAABAAoACQAAAAmui0GsAA==&s=tmLAxiUDMEJKMclLiZGzuUt9BH8_1Z3ptx3Bh1LlzNs=&ts=" +
+          now;
+      </script>
+      <noscript>
+        <img height="1" width="1" style="display:none"
+          src="https://alb.reddit.com/snoo.gif?q=CAAHAAABAAoACQAAAAmui0GsAA==&s=tmLAxiUDMEJKMclLiZGzuUt9BH8_1Z3ptx3Bh1LlzNs=" />
+      </noscript>
+      <!-- DO NOT MODIFY -->
+      <!-- End Reddit Conversion Pixel -->
+      <!-- Facebook Pixel Code -->
+      <script>
+        ! function (f, b, e, v, n, t, s) {
+          if (f.fbq) return;
+          n = f.fbq = function () {
+            n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+          };
+          if (!f._fbq) f._fbq = n;
+          n.push = n;
+          n.loaded = !0;
+          n.version = '2.0';
+          n.queue = [];
+          t = b.createElement(e);
+          t.async = !0;
+          t.src = v;
+          s = b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t, s)
+        }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '362444877957386');
+        fbq('track', 'PageView');
+        @yield('facebook-events')
+      </script><noscript><img height="1" width="1"
+          src="https://www.facebook.com/tr?id=362444877957386&ev=PageView&noscript=1" /></noscript>
+      <!-- End Facebook Pixel Code -->
+  @endif
   @yield('header')
-
 </head>
 
 
-{{-- CHANGE THE BODY BASED IF NAV SHOULD BE HIDDEN --}}
-@if(empty($hide_nav))
-  <body class="relative mb-16 {{ theme('bg-white', 'text-gray-600') }}" style="margin-top: 33px;">
-  @include('layouts.navigation', ["title" => $title ?? null])
-@else
-  <body class="m-inset-top relative {{ theme('bg-white', 'text-gray-600') }}">
-@endif
+  {{-- CHANGE THE BODY BASED IF NAV SHOULD BE HIDDEN --}}
+  @if(empty($hide_nav))
+      <body class="relative mb-16 {{ theme('bg-white', 'text-gray-600') }}" style="margin-top: 33px;">
+        @include('layouts.navigation', ["title" => $title ?? null])
+  @else
+      <body class="m-inset-top relative {{ theme('bg-white', 'text-gray-600') }}">
+  @endif
 
-{{-- ====INSIDE BODY==== --}}
 
-        <input type="checkbox" id="check-sidebar-left" class="hidden">
-        <label for="check-sidebar-left" class="fixed z-2 w-full h-full top-0 left-0 scroll-toggler"
-          style="background-color: black; opacity: 0.7;"></label>
-        
+  <input type="checkbox" id="check-sidebar-left" class="hidden">
 
-        {{-- NAVIGATION --}}
-        @if(empty($hide_nav))
-          
+  <label for="check-sidebar-left" class="fixed z-2 w-full h-full top-0 left-0 scroll-toggler"
+    style="background-color: black; opacity: 0.7;"></label>
+
+  @if(empty($hide_nav))
     <aside class="p-inset-top p-inset-bottom z-2 top-0 left-0 fixed h-full flex flex-col justify-between {{ theme('bg-white', 'border-gray-200') }}">
       <ul>
         @if(Auth::check())
@@ -194,86 +245,99 @@
           </li>
         </ul>
       </aside>
+    @endif
 
-        @endif
+      @yield('page')
 
-        {{-- CONTENT SECTION --}}
-        @yield('page')
-
-
-        <div class="p-3 mx-auto" style="max-width: 960px">
-          @if(!empty($hide_nav) && empty($hide_back))
-            <button onclick="history.back()" class="m-inset-top p-5">
-              <i class="fal fa-chevron-left mr-1"></i>
-              {{ session('back_button') ?? "Back" }}
-            </button>
-          @endif
-            
-          @admin
-          <div class="w-full p-3 mb-3 flex items-center justify-start {{ theme('bg-red') }}">
-              <form action="/app/create" method="post">
-                {{ csrf_field() }}
-                <button type="submit" class="font-bold rounded-full text-sm mr-1 px-5 py-1 text-blue-light {{ theme("bg-white") }}">Add App</button>
-              </form>
-              <a href="/providers/edit" class="font-bold rounded-full text-sm mr-1 px-5 py-1 text-blue-light {{ theme("bg-white") }}">Manage Providers</a>
-              <form action="/app/token" method="post">
-                {{ csrf_field() }}
-                <button type="submit" class="font-bold rounded-full text-sm mr-1 px-5 py-1 text-blue-light {{ theme("bg-white") }}">Update Token</button>
-              </form>
-              <a href="/logs" class="font-bold rounded-full text-sm mr-1 px-5 py-1 text-blue-light {{ theme("bg-white") }}">View Logs</a>
-          </div>
-          @endadmin
-    
-          @yield('content')
-    
-          @if($errors->any())
-            @foreach($errors->all() as $error)
-              @component('components.alert', ["bg" => "red"])
-                {{ $error }}
-              @endcomponent
-            @endforeach
-          @endif
-    
-          @if(Session::has("success"))
-            @component('components.alert', ["bg" => "green"])
-              {{ Session::get("success") }}
-            @endcomponent
-          @endif
-    
-          @if(empty($hide_footer))
-            {{--@include('layouts.footer')--}}
-          @endif
+    <div class="p-3 mx-auto" style="max-width: 960px">
+      @if(!empty($hide_nav) && empty($hide_back))
+        <button onclick="history.back()" class="m-inset-top p-5">
+          <i class="fal fa-chevron-left mr-1"></i>
+          {{ session('back_button') ?? "Back" }}
+        </button>
+      @endif
+        
+      @admin
+      <div class="w-full p-3 mb-3 flex items-center justify-start {{ theme('bg-red') }}">
+          <form action="/app/create" method="post">
+            {{ csrf_field() }}
+            <button type="submit" class="font-bold rounded-full text-sm mr-1 px-5 py-1 text-blue-light {{ theme("bg-white") }}">Add App</button>
+          </form>
+          <a href="/providers/edit" class="font-bold rounded-full text-sm mr-1 px-5 py-1 text-blue-light {{ theme("bg-white") }}">Manage Providers</a>
+          <form action="/app/token" method="post">
+            {{ csrf_field() }}
+            <button type="submit" class="font-bold rounded-full text-sm mr-1 px-5 py-1 text-blue-light {{ theme("bg-white") }}">Update Token</button>
+          </form>
       </div>
+      @endadmin
 
-      <script src="{{ mix('/js/manifest.min.js') }}"></script>
-      <script src="{{ mix('/js/vendor.min.js') }}"></script>
-      <script src="{{ mix('/js/main.min.js') }}"></script>
+      @yield('content')
 
-      @if(empty($hide_ads))
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
+      @if($errors->any())
+        @foreach($errors->all() as $error)
+          @component('components.alert', ["bg" => "red"])
+            {{ $error }}
+          @endcomponent
+        @endforeach
       @endif
 
-      @yield("footer")
+      @if(Session::has("success"))
+        @component('components.alert', ["bg" => "green"])
+          {{ Session::get("success") }}
+        @endcomponent
+      @endif
 
-      {{-- SCRIPT FOR LINKS IN STANDALONE APP --}}
-      <script>
-        (function (a, b, c) {
-          if (c in b && b[c]) {
-            var d, e = a.location,
-              f = /^(a|html)$/i;
-            a.addEventListener("click", function (a) {
-              d = a.target;
-              while (!f.test(d.nodeName)) d = d.parentNode;
-              "href" in d && (d.href.indexOf("http") || ~d.href.indexOf(e.host)) && (a.preventDefault(), e.href = d
-                .href)
-            }, !1)
-          }
-        })(document, window.navigator, "standalone")
-      </script>
+      @if(empty($hide_footer))
+        {{--@include('layouts.footer')--}}
+      @endif
+  </div>
 
 
-{{-- ====END INSIDE BODY==== --}}
+
+  
+
+ 
+  <script src="{{ mix('/js/manifest.min.js') }}"></script>
+  <script src="{{ mix('/js/vendor.min.js') }}"></script>
+  <script src="{{ mix('/js/main.min.js') }}"></script>
+
+  @if(empty($hide_ads))
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+  @endif
+
+  @yield("footer")
+
+  <script>
+    (function (a, b, c) {
+      if (c in b && b[c]) {
+        var d, e = a.location,
+          f = /^(a|html)$/i;
+        a.addEventListener("click", function (a) {
+          d = a.target;
+          while (!f.test(d.nodeName)) d = d.parentNode;
+          "href" in d && (d.href.indexOf("http") || ~d.href.indexOf(e.host)) && (a.preventDefault(), e.href = d
+            .href)
+        }, !1)
+      }
+    })(document, window.navigator, "standalone")
+  </script>
+
+  <script>
+  if ('serviceWorker' in navigator) {
+    console.log("Will the service worker register?");
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(function(reg){
+        console.log("Yes, it did.");
+      }).catch(function(err) {
+        console.log("No it didn't. This happened:", err)
+    });
+  }
+  </script>
+
+
+
 </body>
+
 </html>
---
+++ resources/views/layouts/splash.blade.php
@@ -0,0 +1,54 @@
+{{-- iPad retina portrait 1536x2008 --}}
+<link href="/defaults/launch/2048x2732.png" media="(device-width: 768px) and (device-height: 1024px)
+and (-webkit-device-pixel-ratio: 2)
+and (orientation: portrait)" rel="apple-touch-startup-image">
+
+{{-- iPad retina portrait 1536x2008 --}}
+<link href="/defaults/launch/1536x2008.png" media="(device-width: 768px) and (device-height: 1024px)
+  and (-webkit-device-pixel-ratio: 2)
+  and (orientation: portrait)" rel="apple-touch-startup-image">
+
+{{-- iPad retina landscape 1496x2048 --}}
+<link href="/defaults/launch/1496x2048.png" media="(device-width: 768px) and (device-height: 1024px)
+  and (-webkit-device-pixel-ratio: 2)
+  and (orientation: landscape)" rel="apple-touch-startup-image">
+
+{{-- iPad non-retina portrait 768x1004 --}}
+<link href="/defaults/launch/768x1004.png" media="(device-width: 768px) and (device-height: 1024px)
+  and (-webkit-device-pixel-ratio: 1)
+  and (orientation: portrait)" rel="apple-touch-startup-image">
+
+{{-- iPad non-retina landscape 748x1024 --}}
+<link href="/defaults/launch/748x1024.png" media="(device-width: 768px) and (device-height: 1024px)
+  and (-webkit-device-pixel-ratio: 1)
+  and (orientation: landscape)" rel="apple-touch-startup-image">
+
+{{-- iPhone 6 Plus portrait retina 1242x2148 --}}
+<link href="/defaults/launch/1242x2148.png" media="(device-width: 414px) and (device-height: 736px)
+  and (-webkit-device-pixel-ratio: 3)
+  and (orientation: portrait)" rel="apple-touch-startup-image">
+
+{{-- iPhone 6 Plus landscape retina 1182x2208 --}}
+<link href="/defaults/launch/1182x2208.png" media="(device-width: 414px) and (device-height: 736px)
+  and (-webkit-device-pixel-ratio: 3)
+  and (orientation: landscape)" rel="apple-touch-startup-image">
+
+{{-- iPhone 6 portrait retina 750x1294 --}}
+<link href="/defaults/launch/750x1294.png" media="(device-width: 375px) and (device-height: 667px)
+  and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
+
+{{-- iPhone 5 SE retina 640x1136 --}}
+<link href="/defaults/launch/640x1136.png" media="(device-width: 320px) and (device-height: 568px)
+  and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
+
+{{-- iPhone 5 retina 640x1096 --}}
+<link href="/defaults/launch/640x1096.png" media="(device-width: 320px) and (device-height: 568px)
+  and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
+
+{{-- iPhone < 5 retina 640x920 --}}
+<link href="/defaults/launch/640x920.png" media="(device-width: 320px) and (device-height: 480px)
+  and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
+
+{{-- iPhone < 5 non-retina 320x460 --}}
+<link href="/defaults/launch/320x460.png" media="(device-width: 320px) and (device-height: 480px)
+  and (-webkit-device-pixel-ratio: 1)" rel="apple-touch-startup-image">
