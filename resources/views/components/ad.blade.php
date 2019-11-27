6a93e5ce1def33e4c9fb1414665f575e9d8baa11
Zeb <burroughszeb@me.com>
2019-11-26 23:31:53 -0600

    moved bg of ad to filler.

--- resources/views/components/ad.blade.php
+++ resources/views/components/ad.blade.php
@@ -1,7 +1,7 @@
 
 
-<div class="relative overflow-hidden" style="background-color: lemonchiffon">
-    <div class="absolute left-0 top-0 bottom-0 right-0 p-3">
+<div class="relative overflow-hidden">
+    <div class="absolute left-0 top-0 bottom-0 right-0 p-3" style="background-color: lemonchiffon">
         <div class="absolute bottom-0 right-0 p-3 ">
             <strong>Promoted</strong> <i class="fas fa-ad ml-2"></i>
         </div>
