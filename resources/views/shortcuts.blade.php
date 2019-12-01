e21dc0fa0f2bfa9d3b4d7d4f7dd004b133478f30
Zack <zackbaldwin148@gmail.com>
2019-12-01 13:04:30 -0500

    update shortcut extends

--- resources/views/shortcuts.blade.php
+++ resources/views/shortcuts.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.redesign', ["title" => "Shortcuts"])
+@extends('layouts.redesign', ["title" => "Shortcuts", "hide_nav" => true, "hide_ads" => true])
 
 @section('header')
     <link rel="stylesheet" href="{{ mix('/css/markdown.css') }}">
