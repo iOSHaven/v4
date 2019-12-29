8069a641de56ce6855d1eaec0924339b46626091
Zeb <burroughszeb@me.com>
2019-12-29 13:49:07 -0600

    prevent overlapping cron jobs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('summarize:analytics')
-                 ->everyMinute();
+                 ->everyMinute()->withoutOverlapping();
     }
 
     /**
