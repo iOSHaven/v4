d477d8f8d65b04bf630ee02b5b8d205c6b5cd967
Zeb <burroughszeb@me.com>
2020-01-01 14:38:50 -0600

    trying to fix icon stretch glitch

--- resources/views/components/applayout.blade.php
+++ resources/views/components/applayout.blade.php
@@ -2,7 +2,7 @@
   <div class="flex items-center mb-3">
     <a href="/app/{{ $app->uid }}" class="absolute top-0 left-0 right-0 bottom-0"></a>
     <div class="flex py-1 pointer-events-none flex-grow">
-      <img src="{{ $app->icon }}" width="77" height="77" style="height:77px; border-radius: 1.3rem">
+      <img class="listing-icon" src="{{ $app->icon }}" width="77" height="77">
       <div class="pl-5 w-full relative">
         <div class="{{ theme('text-black') }}">{{ $app->name }}</div>
         <div class="leading-none text-sm {{ theme('text-gray-500') }}">{{ $app->short }}</div>
