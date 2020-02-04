03fae982b266e7646c6811bf837af4a58c60180b
Zeb <burroughszeb@me.com>
2020-02-04 17:31:34 -0600

    better og title for normal pages

--- resources/views/layouts/redesign.blade.php
+++ resources/views/layouts/redesign.blade.php
@@ -11,7 +11,7 @@
 
   {{-- SEARCH ENGINE FRIENDLY --}}
   <meta name="description"
-      content="Discover hacked games, tweaked apps, jailbreaks and more. We offer tools to get started, links you should visit, and thousands of popular apps ready for download.">
+      content="Discover hacked games, tweaked apps, jailbreaks and more. We offer tools to get started, links you should visit, and thousands of popular apps ready for download. It's like PornHub for iOS apps.">
   <meta name="keywords" content="iphone,jailbreak,sideload,hack,crack,signed,download,ipa,free">
   <meta name="robots" content="index, follow">
   <meta name="web_author" content="IOS Haven Development Team">
@@ -21,10 +21,15 @@
   @if(View::hasSection('twitter'))
         @yield('twitter')
   @else
-    <meta property="og:title" content="{{ config('app.name') }}">
+    @php 
+      $pagename = $title ?? session("current_tab") ?? "IOS Haven";
+      $text = config('app.name');
+      $titletext = $pagename ? $text . " - $pagename" : $text;
+    @endphp
+    <meta property="og:title" content="{{ $titletext }}">
     <meta property="og:type" content="article">
     <meta property="og:url" content="{{ $url ?? url('/') }}">
-    <meta property="og:description" content="Its like PornHub for iOS Apps.">
+    <meta property="og:description" content="iOS Haven is like PornHub for iOS Apps.">
     <meta property="og:image" content="https://storage.ihvn.dev/icons/apps/ioshaven.jpg">
     <meta property="twitter:site:id" content="715729557769166848">
   @endif
