fdd29a54069958d3610f778e981f4ed1a4cef88d
Zeb <burroughszeb@me.com>
2020-05-21 20:50:01 -0500

    stuff

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -74,8 +74,15 @@ class AppController extends Controller
             "iconURL" => $ipa->apps[0]['icon'],
             "tintColor" => "018084",
             "size" => $ipa->apps[0]['size'] ?? 0,
-            "screenshotURLs" => [],
-            "permissions" => [],
+            "screenshotURLs" => [
+              "https://user-images.githubusercontent.com/705880/78942028-acf54300-7a6d-11ea-821c-5bb7a9b3e73a.PNG"
+            ],
+            "permissions" => [
+              [
+                "type" => "background-fetch",
+                "usageDescription" => "AltStore periodically refreshes apps in the background to prevent them from expiring."
+              ],
+            ],
           ];
         }
         
