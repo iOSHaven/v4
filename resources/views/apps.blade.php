1beb9f810406589709342d24a3f3d694968dc306
Zeb <burroughszeb@me.com>
2020-06-04 21:22:26 -0500

    hotfix css

--- resources/views/apps.blade.php
+++ resources/views/apps.blade.php
@@ -13,7 +13,7 @@
           <div class="flex items-center mb-3">
             <a href="https://twitter.com/iOSHavencom/status/1267646209084317698" class="absolute top-0 left-0 right-0 bottom-0"></a>
             <div class="flex py-1 pointer-events-none flex-grow rounded-full">
-              <img class="rounded-full" src="https://storage.ihvn.dev/providers/ed85446169d8767546bac065a1ea1b2d94e134ddf41e7309887f5f9a7e896a87.jpeg" width="45" height="45">
+              <img style="width:45px; height:45px;" class="rounded-full" src="https://storage.ihvn.dev/providers/ed85446169d8767546bac065a1ea1b2d94e134ddf41e7309887f5f9a7e896a87.jpeg" width="45" height="45">
               <div class="pl-3 w-full relative">
                 <div class="text-white-dark font-bold">Apple Giftcard Giveaway on Twitter!</div>
                 <div class="leading-none text-sm text-white-dark">$200 of prizes!</div>
