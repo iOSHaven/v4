e3691545dd01bbc862c0b51ccf50895c8d5e1c66
Zeb <burroughszeb@me.com>
2020-06-04 21:20:39 -0500

    twitter announcement

--- resources/views/apps.blade.php
+++ resources/views/apps.blade.php
@@ -8,6 +8,23 @@
 
     <div class="container">
       <div class="row" id="apps">
+
+        <div class="relative bg-yellow-light rounded-full pl-1 pr-3">
+          <div class="flex items-center mb-3">
+            <a href="https://twitter.com/iOSHavencom/status/1267646209084317698" class="absolute top-0 left-0 right-0 bottom-0"></a>
+            <div class="flex py-1 pointer-events-none flex-grow rounded-full">
+              <img class="rounded-full" src="https://storage.ihvn.dev/providers/ed85446169d8767546bac065a1ea1b2d94e134ddf41e7309887f5f9a7e896a87.jpeg" width="45" height="45">
+              <div class="pl-3 w-full relative">
+                <div class="text-white-dark font-bold">Apple Giftcard Giveaway on Twitter!</div>
+                <div class="leading-none text-sm text-white-dark">$200 of prizes!</div>
+              </div>
+            </div>
+            <div class="-ml-4">
+              <i class="fal fa-chevron-right fa-2x {{ theme('text-gray-400') }}"></i>
+            </div>
+          </div>
+        </div>
+
           @foreach($apps as $model)
             @if(class_basename($model) == 'App')
               @component('components.applayout', ["app" => $model])@endcomponent
