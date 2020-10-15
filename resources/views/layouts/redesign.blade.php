2d8c36c98220756cc28d1d6b8569a01da1f42258
Zeb <burroughszeb@me.com>
2020-10-15 14:06:32 -0500

    analytics

--- resources/views/layouts/redesign.blade.php
+++ resources/views/layouts/redesign.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+  {{-- Google Tag Manager --}}
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-PVKPPPZ');</script>
+  {{-- End Google Tag Manager --}}
+
   <title>{{ config('app.name', 'IOS Haven') }}</title>
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -43,8 +52,8 @@
   <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" rel="stylesheet">
   <link href="{{ mix('/css/redesign.min.css') }}" rel="stylesheet">
 
-  {{-- GOOGLE ANALYTICS --}}
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106909262-1"></script>
+  {{-- GOOGLE TAG --}}
+  {{-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106909262-1"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
 
@@ -53,26 +62,26 @@
     }
     gtag('js', new Date());
     gtag('config', 'UA-106909262-1');
-  </script>
+  </script> --}}
 
 
   {{-- GOOGLE ADSENSE --}}
-  @if(empty($hide_ads))
+  {{-- @if(empty($hide_ads))
       <script data-ad-client="ca-pub-4649450952406116" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-  @endif
+  @endif --}}
 
   {{-- REDDIT CONVERSION PIXEL --}}
-  <script>var now = Date.now();var i = new Image();i.src="https://alb.reddit.com/snoo.gif?q=CAAHAAABAAoACQAAAAmui0GsAA==&s=tmLAxiUDMEJKMclLiZGzuUt9BH8_1Z3ptx3Bh1LlzNs=&ts=" + now;</script>
-  <noscript><img height="1" width="1" style="display:none"src="https://alb.reddit.com/snoo.gif?q=CAAHAAABAAoACQAAAAmui0GsAA==&s=tmLAxiUDMEJKMclLiZGzuUt9BH8_1Z3ptx3Bh1LlzNs=" /></noscript>
+  {{-- <script>var now = Date.now();var i = new Image();i.src="https://alb.reddit.com/snoo.gif?q=CAAHAAABAAoACQAAAAmui0GsAA==&s=tmLAxiUDMEJKMclLiZGzuUt9BH8_1Z3ptx3Bh1LlzNs=&ts=" + now;</script>
+  <noscript><img height="1" width="1" style="display:none"src="https://alb.reddit.com/snoo.gif?q=CAAHAAABAAoACQAAAAmui0GsAA==&s=tmLAxiUDMEJKMclLiZGzuUt9BH8_1Z3ptx3Bh1LlzNs=" /></noscript> --}}
 
   {{-- FACEBOOK CONVERSION CODE --}}
-  <script>! function (f, b, e, v, n, t, s) {if (f.fbq) return;n = f.fbq = function () {n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)};if (!f._fbq) f._fbq = n;n.push = n;n.loaded = !0;n.version = '2.0';n.queue = [];t = b.createElement(e);t.async = !0;t.src = v;s = b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t, s)}(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');fbq('init', '362444877957386');fbq('track', 'PageView');
+  {{-- <script>! function (f, b, e, v, n, t, s) {if (f.fbq) return;n = f.fbq = function () {n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)};if (!f._fbq) f._fbq = n;n.push = n;n.loaded = !0;n.version = '2.0';n.queue = [];t = b.createElement(e);t.async = !0;t.src = v;s = b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t, s)}(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');fbq('init', '362444877957386');fbq('track', 'PageView');
     @yield('facebook-events')
   </script>
   <noscript><img height="1" width="1"src="https://www.facebook.com/tr?id=362444877957386&ev=PageView&noscript=1" /></noscript>
 
   <script src="https://cdn.jsdelivr.net/npm/intersection-observer@0.7.0/intersection-observer.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/vanilla-lazyload@17.1.2/dist/lazyload.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/vanilla-lazyload@17.1.2/dist/lazyload.min.js"></script> --}}
 
 
   {{-- PER VIEW HEADER CODE --}}
@@ -307,6 +316,10 @@
 {{-- propeller ad --}}
 <div id="ioshaven-popunder"></div>
 
+{{-- Google Tag Manager (noscript) --}}
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PVKPPPZ"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+{{-- End Google Tag Manager (noscript) --}}
 {{-- ====END INSIDE BODY==== --}}
 </body>
 </html>
--- resources/views/themes.blade.php
+++ resources/views/themes.blade.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+	    {{-- Google Tag Manager --}}
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-PVKPPPZ');</script>
+	{{-- End Google Tag Manager --}}
+
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -210,5 +218,11 @@
 		
 	</script>
 
+
+{{-- Google Tag Manager (noscript) --}}
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PVKPPPZ"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  {{-- End Google Tag Manager (noscript) --}}
+  {{-- ====END INSIDE BODY==== --}}
   </body>
 </html>
