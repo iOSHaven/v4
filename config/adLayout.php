f5a275fff1448c45b00330bc66dc2fe6754932f5
Zeb <burroughszeb@me.com>
2020-11-10 11:32:04 -0600

    ad config adsterra and prop

--- config/adLayout.php
+++ config/adLayout.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    "apps" => "native-banner",
-    "games" => "native-banner",
-    "updates" => "native-banner",
-    "download" => "native-banner",
-    "install" => "native-banner",
+    "apps" => "social-bar",
+    "games" => "social-bar",
+    "updates" => "social-bar",
+    "download" => "adsterra-pop-under",
+    "install" => "adsterra-pop-under",
     "search" => "native-banner",
     "detail" => "native-banner",
 ];
