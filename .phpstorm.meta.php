3a6e1005752bed88a09c9778124628bd6f92c559
Zeb <burroughszeb@me.com>
2020-12-11 23:38:03 -0600

    php unit

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-12-12 04:01:18.
+    * Generated on 2020-12-12 05:37:35.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
