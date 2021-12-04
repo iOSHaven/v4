85827a2e7f700e08cff94c1714382d5750998e9e
zeb <burroughszeb@me.com>
2021-12-04 13:11:08 -0600

    higher in-feed ad

--- resources/views/templates/AppTemplate.blade.php
+++ resources/views/templates/AppTemplate.blade.php
@@ -4,3 +4,8 @@
 @else
 @component('components.shortcut', ["shortcut" => $model])@endcomponent
 @endif
+
+@if($apps->currentPage() <= 3 && $loop->iteration == 3)
+    @component('ads.google-in-feed')@endcomponent
+    @endif
+    @endforeach
