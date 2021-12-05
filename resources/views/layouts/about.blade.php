19bcfec6837654c899a5f57dd72bc555d21f8121
zeb <burroughszeb@me.com>
2021-12-04 19:59:50 -0600

    seo keywords on home page

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -1,28 +1,34 @@
 <section id="#about-us">
-    <div class="markdown">
-      <h2 class="text-shadow">About Us</h2>
-      <p>
-        iOS Haven started out as a small website that just listed a few links to popular tweaks and hacks. The orginal creator, Zane, wanted to make the website bigger, but he didn't know where to start. A few months later, two college students stumbled across the website and saw its potential. Those students, Zack and Zeb, helped Zane take the website to the next level.
+  <div class="text-xl text-leading mt-4">
+    <h2 class="text-shadow text-5xl">About</h2>
+    <p class="mt-4">iOS Haven is a 3rd party app store search engine powered by the people in the jailbreak community. Users submit apps to share with the world and we provide driving sales!</p>
+    <p class="mt-4">With nearly 2000+ submissions and counting, we have next level searching capabilities and mirroring. It's like insurance!</p>
+    <p class="mt-4">Anyone with iPhone or Apple Mac can make use of our tools. Truly, iOS Haven is for those seeking power through apps and shortcuts.
+      Our competitors provide great services, but nobody provides support and knowledge like iOS Haven and its partners. Contact us anytime.</p>
+    <p class="mt-4">People who choose to create an account will gain early access to upcoming features. Including private and limited access to partner projects!</p>
+    <p class="mt-4">Smart people use the automated systems of iOS Haven to bank on quality insights.</p>
+    <!-- <p>
+        iOS Haven started out as a small website that just listed a few links to popular tweaks and hacks. The original creator, Zane, wanted to make the website bigger, but he didn't know where to start. A few months later, two college students stumbled across the website and saw its potential. Those students, Zack and Zeb, helped Zane take the website to the next level.
       </p>
-
       <p>
         Since then, we have added over 1000 apps. Those apps can be installed directly from the web browser or can be downloaded as an IPA archive.
       </p>
 
       <p>
-        Along the way, our goal has been to make it easy to access tools/apps. We want to make all information publically available to anyone that wants to use their iPhone to the fullest. 
+        Along the way, our goal has been to make it easy to access tools/apps. We want to make all information publicly available to anyone that wants to use their iPhone to the fullest. 
       </p>
 
       <p>
         Unlike other websites in the community, we do not require you to install our app, make an account, or pay money. Likewise, we do not share your information or use your device for cryptocurrency mining. We like to put the user first and we always will.
-      </p>
+      </p> -->
 
-    </div>
-  </section>
+  </div>
+</section>
 
-  <section>
-    <div class="markdown">
-      <h2 class="text-shadow">Redesign</h2>
-      <p>We are trying to redesign IOS Haven. However, both of us are in college so we do not have as much time to work on the website. If you encounter any issues please contact us via Twitter or Discord and we will try to fix it as soon as possible. </p>
-    </div>
-  </section>
+<section>
+  <div class="text-xl text-leading mt-4">
+    <h2 class="text-shadow text-5xl">Redesign</h2>
+    <p class="mt-4">New features coming soon! We are redesigning and creating smart systems from the ground up. Please notify the developers or private beta testers if you encounter any issues. All bugs will be resolved as quick as possible.</p>
+    <!-- <p>We are trying to redesign IOS Haven. However, both of us are in college so we do not have as much time to work on the website. If you encounter any issues please contact us via Twitter or Discord and we will try to fix it as soon as possible. </p> -->
+  </div>
+</section>
