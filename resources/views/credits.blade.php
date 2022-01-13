284a710987dd374eda7580d729072da9391fec09
zeb <burroughszeb@me.com>
2022-01-13 08:18:49 -0600

    update credits page

--- resources/views/credits.blade.php
+++ resources/views/credits.blade.php
@@ -6,32 +6,34 @@
 
 @section('content')
 
-<div class="markdown">
-  <h3>Developers</h3>
+<div>
+  <div class="markdown">
+    <h3>Developers</h3>
+  </div>
+
   <ul>
     <li class="flex items-center justify-center my-1">
-      <div class="w-2/3">Zeb - Software Engineer</div>
+      <div class="w-2/3">Zeb - Networking & Software Engineer</div>
       <div class="w-1/3 text-right">
-        <!-- <a href="https://www.reddit.com/user/zebthewizard/" target="_blank"><i class="fab fa-reddit"></i></a> -->
         <a href="https://twitter.com/wizardzeb" target="_blank"><i class="fab fa-twitter fa-lg"></i></a>
       </div>
     </li>
     <li class="flex items-center justify-center my-1">
-      <div class="w-2/3">Zack - Site Maintainer
+      <div class="w-2/3">Zack - Business & Social Media Executive
       </div>
       <div class="w-1/3 text-right">
        <a href="https://twitter.com/_ZackBz" target="_blank"><i class="fab fa-twitter fa-lg"></i></a>
       </div>
     </li>
     <li class="flex items-center justify-center my-1">
-      <div class="w-2/3">Matt - Social Media
+      <div class="w-2/3">Smile - Shortcut Enthusiast, Japanese Translator, and Student Helper
       </div>
       <div class="w-1/3 text-right">
-       <a href="https://twitter.com/mattactular" target="_blank"><i class="fab fa-twitter fa-lg"></i></a>
+       <a href="https://twitter.com/Smiledayo_" target="_blank"><i class="fab fa-twitter fa-lg"></i></a>
       </div>
     </li>
     <li class="flex items-center justify-center my-1">
-      <div class="w-2/3">Nick - Nerk
+      <div class="w-2/3">Nick - Game Enthusiast
       </div>
       <div class="w-1/3 text-right">
        <a href="https://discord.gg/kK4TJBf" target="_blank"><i class="fab fa-discord fa-lg"></i></a>
@@ -75,16 +77,23 @@
   ];
 ?>
 
-<div class="markdown">
-  <h3>App Providers</h3>
+<div class="">
+  <div class="markdown">
+    <h3>App Providers</h3>
+  </div>
+
   <ul>
-    @foreach($app_providers as $provider)
+    @foreach(\App\Provider::get() as $provider)
+{{--      @dump($provider)--}}
     <li class="flex items-center justify-center my-1">
-      <div class="w-2/3">{{ $provider["name"] }}</div>
+      <div class="w-2/3 flex items-center">
+          <x-tinyProviderIcon :src="$provider->avatar" class="mr-2"></x-tinyProviderIcon>
+          <div>{{ $provider->name }}</div>
+      </div>
       <div class="w-1/3 text-right">
-        @foreach($provider["links"] as $icon => $link)
-          <a href="{{ $link }}" target="_blank"><i class="{{ $icon }} fa-lg"></i></a>
-        @endforeach
+{{--        @foreach($provider["links"] as $icon => $link)--}}
+          <a href="{{ $provider->website }}" target="_blank"><i class="fal fa-globe fa-lg"></i></a>
+{{--        @endforeach--}}
       </div>
     </li>
     @endforeach
