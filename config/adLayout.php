c7bb2da0bd43f3968047925f409a5763676352d8
zack <zb01406@gmail.com>
2022-03-26 20:57:58 -0400

    Update Ads

--- config/adLayout.php
+++ config/adLayout.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-     "apps" => "admaven-floater",
-     "games" => "admaven-floater",
+     "apps" => "admaven-native-floater",
+     "games" => "admaven-native-floater",
      "updates" => "admaven-floater",
      "download" => "adsterra-pop-under",
      "install" => "adsterra-pop-under",
 
     // contains additional code on blade templates.
-     "search" => "admaven-pop-under",
-     "detail" => "admaven-pop-under",
+     "search" => "admaven-popunder",
+     "detail" => "admaven-popunder",
 ];
