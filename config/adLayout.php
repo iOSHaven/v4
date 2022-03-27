302873d9e86113007abc40b91a3957e21273f131
zack <zb01406@gmail.com>
2022-03-27 11:06:57 -0400

    Ads

--- config/adLayout.php
+++ config/adLayout.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-     "apps" => "prop-push-notifications",
-     "games" => "prop-push-notifications",
-     "updates" => "prop-push-notifications",
+     "apps" => "admaven-native-floater",
+     "games" => "admaven-native-floater",
+     "updates" => "admaven-floater",
      "download" => "adsterra-pop-under",
      "install" => "adsterra-pop-under",
 
     // contains additional code on blade templates.
-     "search" => "prop-push-notifications",
-     "detail" => "prop-push-notifications",
+     "search" => "admaven-popunder",
+     "detail" => "admaven-popunder",
 ];
