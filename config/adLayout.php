ee6955f9c9eb7243eee4f7d431d4d97dbf04f622
zack <zb01406@gmail.com>
2022-04-15 06:57:38 -0400

    Adds changes to adLayout

--- config/adLayout.php
+++ config/adLayout.php
@@ -4,8 +4,8 @@ return [
      "apps" => "admaven-native-floater",
      "games" => "admaven-native-floater",
      "updates" => "admaven-native-floater",
-     "download" => "adsterra-pop-under",
-     "install" => "adsterra-pop-under",
+     "download" => "adadmaven-popunder",
+     "install" => "adadmaven-popunder",
 
     // contains additional code on blade templates.
      "search" => "adadmaven-popunder",
