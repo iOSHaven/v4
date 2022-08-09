88dac8b6910f0f84e33f16bf5a83c05d66d0ba07
finn <finn@zebs-MacBook-Air.local>
2022-08-09 18:55:14 -0500

    remove dead code in new views

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -3,12 +3,6 @@
 @section('content')
 
 
-
-{{-- <div class="px-24 mb-3">
-  <img src="/SVG/login.svg" class="w-full" alt="">
-</div>- --}}
-
-
 <section>
   <div class="max-w-[50ch] mx-auto">
     {{-- <h3 class="mt-0 text-3xl font-display">Login</h3> --}}
@@ -57,28 +51,6 @@
     </form>
   </div>
 </section>
-
-{{-- <div class="container pt-3">
-  <div class="row">
-    <div class="col-tablet-portrait-5 mx-auto flex-cc">
-      <a class="btn col-tablet-portrait-6 py-1 border-none btn btn-dark disabled">Login</a>
-      <a class="btn col-tablet-portrait-6 py-1 border-none btn btn-white text-dark" href="/register">Sign up</a>
-    </div>
-  </div>
-  <div class="row mt-3">
-    <div class="col-tablet-portrait-5 mx-auto">
-      <form action="{{ route('login') }}" method="post">
-{{ csrf_field() }}
-<input type="text" class="p-3 {{ $errors->has('username') ? ' has-error' : '' }}" value="{{ old('username') }}"
-  name="username" placeholder="username" required autofocus>
-<input type="password" class="p-3 {{ $errors->has('password') ? ' has-error' : '' }}" name="password"
-  placeholder="password" required>
-<button type="submit" class="p-3 btn btn-blue col-12 mt-3">Login</button>
-</form>
-</div>
-
-</div>
-</div> --}}
 
 
 @endsection
