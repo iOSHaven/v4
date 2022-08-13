961d03305b1ae623aa7c13b6810db0f9235037cc
finn <finn@zebs-MacBook-Air.local>
2022-08-13 00:03:15 -0500

    make summary analytic models fillable

--- app/Summary/SummaryDownload.php
+++ app/Summary/SummaryDownload.php
@@ -8,6 +8,10 @@ class SummaryDownload extends Model
 {
     protected $table = 'summary_download';
 
+    protected $fillable = [
+        "trigger_id", "trigger_type", "amount", "created_at"
+    ];
+
     public function trigger()
     {
         return $this->morphTo();
