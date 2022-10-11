39318550c5cdcf607944404409e956942777c1ea
Zeb <burroughszeb@me.com>
2022-10-10 19:35:20 -0500

    null check

--- resources/views/components/tinyProviderIcon.blade.php
+++ resources/views/components/tinyProviderIcon.blade.php
@@ -1,3 +1,3 @@
 <span class="inline relative rounded-full overflow-hidden {{ $class ?? 'mr-1' }}">
-    <img class="rounded-full" src="{{ $src ?? $provider->avatar }}" alt="" width="{{ $size ?? '20' }}">
+    <img class="rounded-full" src="{{ $src ?? $provider?->avatar ?? 'defaults/icon.png' }}" alt="" width="{{ $size ?? '20' }}">
 </span>
