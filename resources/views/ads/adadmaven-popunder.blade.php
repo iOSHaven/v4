9252b5fef405c5cbc81c2997e4a16ac1f588eade
Zeb <burroughszeb@me.com>
2022-11-05 16:12:57 -0500

    uses existing admaven-pop-under file

--- resources/views/ads/adadmaven-popunder.blade.php
+++
@@ -1,2 +0,0 @@
-<!-- <script data-cfasync="false" src="//d18e74vjvmvza1.cloudfront.net/?vjved=953394"></script> -->
-<script data-cfasync="false" src="//d3vw4uehoh23hx.cloudfront.net/?euwvd=953394"></script>
