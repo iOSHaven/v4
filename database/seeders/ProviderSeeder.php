2f607754f8e610b54fe1f2f9cf1243e76533fe53
Zeb <burroughszeb@me.com>
2023-06-03 16:57:19 -0500

    provider seeder

---
+++ database/seeders/ProviderSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Ipa;
+use App\Models\Itms;
+use App\Models\Provider;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProviderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (! Itms::count()) {
+            Itms::factory(300);
+        }
+
+        if (! Ipa::count()) {
+            Ipa::factory(300);
+        }
+
+        Provider::factory(10)
+            ->withExistingIpas()
+            ->withExistingItms()
+            ->create();
+    }
+}
