738969fd473ab1ac7e0909e194012848de93bc47
Zeb <burroughszeb@me.com>
2023-06-07 22:44:04 -0500

    hotfix shortcut images

--- resources/views/components/app-icon.blade.php
+++ resources/views/components/app-icon.blade.php
@@ -1,6 +1,6 @@
 <div style="width: 77px" class="relative ">
     <div class="listing-icon absolute inset-0 aspect-w-1 aspect-h-1 shadow-neutral-300 dark:shadow-black shadow-lg ">
-        <img class="absolute inset-0 rounded-icon lazy-image" data-src={{ $src }} data-srcset="{{ $srcset }}" />
+        <img class="absolute inset-0 rounded-icon lazy-image" data-src={{ $src ?? '' }} data-srcset="{{ $srcset ?? '' }}" />
         <div class="absolute inset-0 bg-black opacity-0"></div>
     </div>
 </div>
